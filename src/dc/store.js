b615a45b9c50df6e2a297f1da7e865ed4dc551fa
lingyun <lingyun@yonyou.com>
2019-08-07 14:20:09 +0800

    添加一点优化

--- src/dc/store.js
+++ src/dc/store.js
@@ -6,7 +6,7 @@ import {
 } from './h';
 
 // 数据源
-const data = {};
+const sourceData = {};
 // 存储model实例
 const model = {};
 // 存储model对应的类
@@ -16,24 +16,53 @@ const syncMethods = {};
 // 存储异步方法
 const asyncMethods = {};
 
+// 缓存，闲时插入sourceData
+const cache = {};
+// 定时器
+const timer = {};
+// 当前数据时间戳
+const storeDate = {};
+
+function storeCache(key) {
+
+    clearTimeout(timer[key]);
+    timer[key] = setTimeout(() => {
+        if(key in cache) {
+            sourceData[key] = deepCopy(cache[key]);
+            delete cache[key];
+        }
+    }, 1000);
+
+}
+
 export default  {
     // 合并数据
     setData(modelName, obj) {
-        data[modelName] = deepCopy(obj);
+        cache[modelName] = obj;
+        storeDate[modelName] = new Date().getTime();
+        storeCache(modelName);
     },
 
     // 返回数据，返回的数据经过拷贝，并非头部对象，以此彻底隔绝别人通过返回数据修改data对象
-    getData(modelName, key) {
+    getData(modelName) {
         if(!(modelName in model)) {
+            let data = Object.assign(sourceData, cache);
             return deepCopy(data);
         }
 
-        if(key !== undefined) {
-            return deepCopy(data[modelName][key]);
+        if(modelName in cache) {
+            return cache[modelName];
         }
-        else {
-            return deepCopy(data[modelName]);
+        return deepCopy(sourceData[modelName]);
+    },
+
+    // 获取某块数据存储时间戳
+    getStoreDate(modelName) {
+        if(!modelName) {
+            throw new Error('no model name');
         }
+
+        return storeDate[modelName]
     },
     
     // 存储model
@@ -41,8 +70,6 @@ export default  {
         if('name' in m) {
             model[m.name] = m;
             this.setData(m.name, m.data);
-            syncMethods[m.name] = model.sync;
-            asyncMethods[m.name] = model.async;
         }
     },
 
